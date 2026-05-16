34894d9f29f6ff944acbc79b8164f0254f1f20e3
DevM0111 <github minoraiacount>
2026-05-16 20:36:09 +0900

    af

--- audio-decode-patch.js
+++ audio-decode-patch.js
@@ -1,37 +1,85 @@
 (function () {
+  var contexts = [];
+
+  function makeSilentBuffer(ctx) {
+    var rate = ctx.sampleRate || 44100;
+    var frames = Math.max(1, (rate / 20) | 0);
+    return ctx.createBuffer(1, frames, rate);
+  }
+
+  function wrapCtor(Orig) {
+    if (!Orig || Orig.__unityWebglPatchedCtor) return Orig;
+    function PatchedAudioContext() {
+      var ctx = new Orig();
+      contexts.push(ctx);
+      return ctx;
+    }
+    PatchedAudioContext.prototype = Orig.prototype;
+    PatchedAudioContext.__unityWebglPatchedCtor = true;
+    return PatchedAudioContext;
+  }
+
   function patchWebAudioDecode() {
     var Ctor = window.AudioContext || window.webkitAudioContext;
     if (!Ctor || !Ctor.prototype || Ctor.prototype.__unityWebglAudioPatched) return;
+
+    if (window.AudioContext) window.AudioContext = wrapCtor(window.AudioContext);
+    if (window.webkitAudioContext) window.webkitAudioContext = wrapCtor(window.webkitAudioContext);
+    Ctor = window.AudioContext || window.webkitAudioContext;
+
     var nativeDecode = Ctor.prototype.decodeAudioData;
     Ctor.prototype.decodeAudioData = function (audioData, successCallback, errorCallback) {
       var ctx = this;
-      var onSuccess = function (buffer) {
+
+      function deliverSuccess(buffer) {
         if (typeof successCallback === "function") successCallback(buffer);
         return buffer;
-      };
-      var onFailure = function (err) {
+      }
+
+      function deliverFailure(err) {
         console.warn("[WebGL] decodeAudioData failed; using silent buffer.", err);
         try {
-          var rate = ctx.sampleRate || 44100;
-          var frames = Math.max(1, (rate / 20) | 0);
-          var silent = ctx.createBuffer(1, frames, rate);
-          return onSuccess(silent);
+          return deliverSuccess(makeSilentBuffer(ctx));
         } catch (inner) {
           if (typeof errorCallback === "function") errorCallback(err);
           throw err;
         }
-      };
+      }
+
       try {
-        var ret = nativeDecode.call(ctx, audioData, onSuccess, onFailure);
+        if (arguments.length <= 1) {
+          var promiseOnly = nativeDecode.call(ctx, audioData);
+          if (promiseOnly && typeof promiseOnly.then === "function") {
+            return promiseOnly.then(deliverSuccess, deliverFailure);
+          }
+          return promiseOnly;
+        }
+
+        var ret = nativeDecode.call(ctx, audioData, deliverSuccess, deliverFailure);
         if (ret && typeof ret.then === "function") {
-          return ret.then(onSuccess, onFailure);
+          return ret.then(deliverSuccess, deliverFailure);
         }
         return ret;
       } catch (err) {
-        return onFailure(err);
+        if (arguments.length <= 1) {
+          return Promise.resolve(makeSilentBuffer(ctx));
+        }
+        return deliverFailure(err);
       }
     };
     Ctor.prototype.__unityWebglAudioPatched = true;
   }
+
+  window.resumeUnityWebAudioFromGesture = function () {
+    for (var i = 0; i < contexts.length; i++) {
+      var ctx = contexts[i];
+      if (ctx && ctx.state === "suspended") {
+        try {
+          ctx.resume();
+        } catch (e) {}
+      }
+    }
+  };
+
   patchWebAudioDecode();
 })();
